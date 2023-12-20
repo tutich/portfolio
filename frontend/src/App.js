a3ffb8650ff095a24fe6e263d791e2cac17e9b32
tutich <pkoinange02@gmail.com>
2023-12-20 23:48:48 +0300

    project files,jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Resume from './components/Resume';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -8,6 +14,11 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/resume' element={<Resume />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/skills' element={<Skills />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
       </Routes>
       </BrowserRouter>
     </div>
