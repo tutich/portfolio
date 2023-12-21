e134be52474fa0238e034b7330a31cd2b631a791
tutich <pkoinange02@gmail.com>
2023-12-21 22:26:10 +0300

    project imgs

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
 
+import homPic from '../images/20231221_212926.jpg'
 
 function Home() {
     return (
-        <>Home</>
+        <div>
+        <h2>Hey, I'm Peter.</h2>
+        <h2>Front End Developer</h2>
+        <img src={homPic} alt=''/>
+        </div>
     )
 }
 
