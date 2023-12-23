d3991a0f3858a29120f240b03da50f1b19b97163
tutich <pkoinange02@gmail.com>
2023-12-23 22:54:54 +0300

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const data = [
+    {
+        label : 'HOME',
+        to : '/'
+    
+    },
+
+    {
+        label : 'ABOUT ME',
+        to : '/about'
+    
+    },
+    
+    {
+        label : 'SKILLS',
+        to : '/skills'
+    
+    },
+
+    {
+        label : 'PORTFOLIO',
+        to : '/portfolio'
+    
+    },
+
+    {
+        label : 'CONTACT',
+        to : '/contact'
+    
+    }
+]
+
+
+
+
+
+
+
+
+function Navbar () {
+    return (
+
+    )
+}
+
+export default Navbar
