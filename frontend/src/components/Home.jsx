82ad2a1a298b05d93092165566d258b32413842c
tutich <pkoinange02@gmail.com>
2023-12-30 16:52:58 +0300

    contact navigate

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import homPic from '../images/20231221_212926.jpg'
 
 function Home() {
+    const navigate = useNavigate();
+    console.log(navigate)
+
+const handleHomeContact = () => {
+       navigate ('/contact' )
+};
+
     return (
-        <div>
-        <h2>Hey, I'm Peter.</h2>
-        <h2>Front End Developer</h2>
-        <img src={homPic} alt=''/>
+        <div className=''>
+          <h2>Hey, I'm Peter.</h2>
+          <h2>Front End Developer</h2>
+          <img src={homPic} alt=''/>
+
+          <div>
+            <button className='home-cont-butn' onClick={handleHomeContact}>Hire me</button>
+          </div>
         </div>
+        
+
+       
     )
 }
 
