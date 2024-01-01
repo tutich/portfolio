5205dfa3b7a702ff3e0b294c30e4371420edff47
tutich <pkoinange02@gmail.com>
2024-01-01 20:58:44 +0300

    media links

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,8 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
+
 
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <div>
+            <h3>Let's Talk</h3>
+            <div>
+                <form action="">
+                    <input
+                    placeholder="Name"
+                    type="text"
+                    />
+                    <input
+                    placeholder="Email"
+                    type="email"
+                    />
+                    <input
+                    placeholder="Say Something"
+                    type="text"
+                    />
+                                   <input
+                    type="Submit"
+                    />
+                </form>
+            </div>
+            <div>
+                <a href='https://www.instagram.com/'><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+                <a href='https://twitter.com/home'><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+                <a href='https://www.linkedin.com'><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+                <a href='https://github.com/tutich'><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+            </div>
+        </div>
     )
 }
 
